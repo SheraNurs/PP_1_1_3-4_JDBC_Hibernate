fd1cb70cc054ea3d2d1801d77c729f09e38d40cb
Nursultan <sheralievnursultan1@gmail.com>
2022-10-06 19:09:21 +0600

    Add UserDaoHibernateImpl.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -11,11 +11,14 @@ public class Main {
         UserDao userDao = new UserDaoJDBCImpl();
 
         userDao.createUsersTable();
+        //userDao.dropUsersTable();
 
-        userDao.saveUser("Name1", "LastName1", (byte) 20);
-        userDao.saveUser("Name2", "LastName2", (byte) 25);
-        userDao.saveUser("Name3", "LastName3", (byte) 31);
-        userDao.saveUser("Name4", "LastName4", (byte) 38);
+        userDao.saveUser("Nurs", "Alex", (byte) 26);
+        userDao.saveUser("Ivan", "Petrov", (byte) 23);
+
+        userDao.removeUserById(1);
+        System.out.println(userDao.getAllUsers());
+        userDao.cleanUsersTable();
 
 
     }
